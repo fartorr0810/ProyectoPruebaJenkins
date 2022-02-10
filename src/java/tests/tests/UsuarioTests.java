a62fa74ed739ea1639bdd6bfd631a9a67eb1e6fb
farrtor0810 <55539017+fartorr0810@users.noreply.github.com>
2022-02-10 10:59:28 +0100

    Update UsuarioTests.java

--- src/java/tests/tests/UsuarioTests.java
+++ src/java/tests/tests/UsuarioTests.java
@@ -18,7 +18,7 @@ class UsuarioTests {
 	@Test
 	void test() {
 		assertEquals("Jonh Cena", user.getUsername());
-		assertEquals("TITIRTIII", user.getPassword());
+		assertEquals("TITIRITIII", user.getPassword());
 	}
 	
 }
