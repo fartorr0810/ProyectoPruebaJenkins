3776f412b0baaa1d3b58ba86d9d02d610e854dde
Fran <frankarroyop@gmail.com>
2022-02-18 10:52:49 +0100

    Selenium terminado

--- src/test/java/test/SeleniumTest.java
+++ src/test/java/test/SeleniumTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,13 +23,19 @@ public class SeleniumTest {
 		driver=new ChromeDriver(options);		
 	}
 	@Test
-	public void Pantalla() {
+	public void Pantalla() throws InterruptedException {
 		driver.get("http://localhost:8888/");
 		WebElement user=driver.findElement(By.id("user"));
-		WebElement password=driver.findElement(By.id("user"));
+		WebElement password=driver.findElement(By.id("password"));
 		WebElement ok=driver.findElement(By.id("login"));
 		
-
+		user.sendKeys("Jorge");
+		Thread.sleep(4000);
+		password.sendKeys("profe12");
+		Thread.sleep(4000);
+		ok.click();
+		String titulo=driver.getCurrentUrl();
+		assertTrue(titulo.equals("http://localhost:8888/login/submit"));
 		
 	}
 	
